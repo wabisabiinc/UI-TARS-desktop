f94ff173f095400122c8f9a1ea55ff9f1cfbf33e
yuto fukuda <fukuda@wbisbi.co.jp>
2025-06-05 08:34:26 +0000

    Update file server.mjs

--- apps/agent-tars/server.mjs
+++ apps/agent-tars/server.mjs
@@ -2,10 +2,12 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-const __filename = fileURLToPath(import.meta.url);
+
+const app = express();
+
 
 app.use(express.static(path.resolve(__dirname, 'dist/web')));
 
