ae542669671f7fc6a1dcdd07845383a046abfc87
yuto fukuda <fukuda@wbisbi.co.jp>
2025-06-06 02:03:04 +0000

    Update file server.mjs

--- apps/agent-tars/server.mjs
+++ apps/agent-tars/server.mjs
@@ -1,9 +1,9 @@
+// apps/agent-tars/server.mjs
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 const app = express();
@@ -11,12 +11,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/web')));
 
-app.get('/.*/', (req, res) => {
+
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist/web/index.html'));
 });
 
+
 const port = process.env.PORT || 4173;
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
