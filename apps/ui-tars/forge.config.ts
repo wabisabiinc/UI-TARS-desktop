a6be12d1e449e6d64daed3dca1efc67e0a33ba7c
uranusfukuda <fukuda@wbisbi.co.jp>
2025-06-17 20:10:00 +0900

    apps/ui-tars/forge.config.tsの変更

--- apps/ui-tars/forge.config.ts
+++ apps/ui-tars/forge.config.ts
@@ -188,7 +188,7 @@ const config: ForgeConfig = {
     {
       name: '@electron-forge/publisher-github',
       config: {
-        repository: { owner: 'bytedance', name: 'ui-tars-desktop' },
+        repository: { owner: 'wabisabiinc', name: 'UI-TARS-desktop' },
         draft: true,
         force: true,
         generateReleaseNotes: true,
